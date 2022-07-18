878d290fe0c3c3015018ba98f2e7d626280d3b6e
squaretus <p.nedovodin@yandex.ru>
2022-07-18 23:36:59 +0300

    fixed another group of bugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  has_many :comments, :dependent => :destroy
+  has_many :comments, dependent: :destroy
 
-  has_many_attached :pictures, :dependent => :destroy
+  has_many_attached :pictures, dependent: :destroy
 
   validates :title, presence: true, length: {minimum: 3}
   validates :body, presence: true, length: {minimum: 10}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, :dependent => :destroy
-  has_many :comments, :dependent => :destroy
- 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
